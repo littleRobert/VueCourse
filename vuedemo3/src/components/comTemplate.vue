0db9fc928589ff1e21029283968c019cded26516
fangy <282735445@qq.com>
2018-05-16 08:44:22 +0800

    update

---
+++ vuedemo3/src/components/comTemplate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+<script>
+
+export default {
+  //自定义组件
+  components: {
+
+  },
+  //变量
+  data () {
+    return {
+
+    }
+  },
+  //获取自定义属性的值
+  props: {
+
+  },
+  //方法
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
